f2c6933411bbf487b1a561bc2538b88a4c22610d
joaopedro-dev <dev_joaopedro@outlook.com>
2023-04-22 00:05:23 -0300

    Primeiro Commit Official | Rumo a viória 🚀

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,36 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Relax Kings</h1>
+      <Image
+      src="/mind.gif"
+      alt="Logo"
+      width={300}
+      height={300}
+      />
+      <div className={styles.blocoHome}>
+
+        <button className={styles.btnBook}>
+          <Link target='blank' href="https://drive.google.com/drive/folders/1joolFsJnvBiPCoYW5dkF7kLI-QCX4vPp?usp=sharing">
+            RELAXE E BAIXE NOSSO EBOOK FREE  
+          </Link>
+        </button>
+        <button className={styles.btnBook}>
+        <Link target='blank' href="https://docs.google.com/forms/d/e/1FAIpQLSfNeB_9q2M8FM08Lm8Kyoijf0XsDaHFi_e9_TTyv0NNX7bwuA/viewform?usp=sf_link">
+            PREENCHA NOSSO FORMULÁRIO  
+        </Link>
+        </button> 
+        </div>
+      <iframe src="https://open.spotify.com/embed/playlist/7pZQ7PBgcjeuihA6k0JzKB?utm_source=generator" width="320px" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+      <footer className={styles.foot}>
+        <Link href="https://www.buymeacoffee.com/relaxkings">
+          Buy us a coffee
+        </Link>
+      </footer>
     </main>
   )
 }
